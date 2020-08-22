efa9b9f7e1d831146f5f93f1f0118a5301445810
Nikko <nikkosolon24@gmail.com>
2020-08-21 22:47:35 -0700

    Laid out content in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ import './styles/main.css';
 function App() {
   return (
     <div className="flex flex-col justify-center items-center text-white">
-      <div className="bg-blue-900">Hello! This is a template stack of React typescript and tailwind</div>
+      <div className="bg-blue-900">Splash Page</div>
+      <div className="bg-blue-800">My Room</div>
+      <div className="bg-blue-700">Check me out</div>
+      <div className="bg-blue-600">Pictures</div>
+      <div className="bg-blue-500">Education/Experience</div>
+      <div className="bg-blue-400">Stats(optional)</div>
+      <div className="bg-blue-300">Footer</div>
     </div>
   );
 }
