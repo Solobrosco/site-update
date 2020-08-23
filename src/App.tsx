580b399e36c06bf5af5b857195080fd069a6d697
Nikko <nikkosolon24@gmail.com>
2020-08-22 20:47:31 -0700

    Imported Navbar.tsx into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './styles/main.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center text-white">
-      <div className="bg-blue-900">Splash Page</div>
-      <div className="bg-blue-800">My Room</div>
-      <div className="bg-blue-700">Check me out</div>
-      <div className="bg-blue-600">Pictures</div>
-      <div className="bg-blue-500">Education/Experience</div>
-      <div className="bg-blue-400">Stats(optional)</div>
-      <div className="bg-blue-300">Footer</div>
+    <div className="flex flex-col justify-center items-center">
+      <NavBar />
+      <div className="bg-blue-900 text-white">Splash Page</div>
+      <div className="bg-blue-800 text-white">My Room</div>
+      <div className="bg-blue-700 text-white">Check me out</div>
+      <div className="bg-blue-600 text-white">Pictures</div>
+      <div className="bg-blue-500 text-white">Education/Experience</div>
+      <div className="bg-blue-400 text-white">Stats(optional)</div>
+      <div className="bg-blue-300 text-white">Footer</div>
     </div>
   );
 }
