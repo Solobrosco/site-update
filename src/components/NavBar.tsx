9bbb4c68e6b07128dad636929bc9c0ee6415ae25
Nikko <nikkosolon24@gmail.com>
2020-08-22 21:47:01 -0700

    Updated NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,9 +2,19 @@ import React from 'react';
 
 const NavBar = () => {
     return(
-        <div>
-            NavBar
-        </div>
+        <header className="flex items-center justify-between py-4 px-10 bg-gray-900">
+            <div className='flex text-2xl h-10 text-white'>
+                Nikko Solon
+            </div>
+            <div>
+                <button type='button' className='block text-white'>
+                    {/* Icon provided by Freepik */}
+                    <svg className='h-6 w-6 fill-current' id="Layer_1" enable-background="new 0 0 512 512" height="512" viewBox="0 0 512 512" width="512" xmlns="http://www.w3.org/2000/svg">
+                        <path d="m464.883 64.267h-417.766c-25.98 0-47.117 21.136-47.117 47.149 0 25.98 21.137 47.117 47.117 47.117h417.766c25.98 0 47.117-21.137 47.117-47.117 0-26.013-21.137-47.149-47.117-47.149z"/><path d="m464.883 208.867h-417.766c-25.98 0-47.117 21.136-47.117 47.149 0 25.98 21.137 47.117 47.117 47.117h417.766c25.98 0 47.117-21.137 47.117-47.117 0-26.013-21.137-47.149-47.117-47.149z"/><path d="m464.883 353.467h-417.766c-25.98 0-47.117 21.137-47.117 47.149 0 25.98 21.137 47.117 47.117 47.117h417.766c25.98 0 47.117-21.137 47.117-47.117 0-26.012-21.137-47.149-47.117-47.149z"/>
+                    </svg>
+                </button>
+            </div>
+        </header>
     );
 }
 
