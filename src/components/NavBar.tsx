11bdc49843caef0e9585aff1bdd28a104d9264a5
Nikko <nikkosolon24@gmail.com>
2020-08-24 22:40:48 -0700

    Added dropdown menu to Navbar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const NavBar = () => {
+    const [open, setOpen] = useState(false);
+
     return(
-        <header className="flex items-center justify-between py-4 px-10 bg-gray-900">
-            <div className='flex text-2xl h-10 text-white'>
-                Nikko Solon
+        <header className='bg-gray-900'>
+            <div className="flex items-center justify-between py-4 px-10">
+                <div className='text-2xl block h-5 text-white'>
+                    Nikko A. Solon
+                </div>
+                <div>
+                    <button type='button' className='block text-white' onClick={() => setOpen(!open)}>
+                        {/* Icon provided by Freepik */}
+                        <svg className='h-6 w-6 fill-current' id="Layer_1" enable-background="new 0 0 512 512" height="512" viewBox="0 0 512 512" width="512" xmlns="http://www.w3.org/2000/svg">
+                            <path d="m464.883 64.267h-417.766c-25.98 0-47.117 21.136-47.117 47.149 0 25.98 21.137 47.117 47.117 47.117h417.766c25.98 0 47.117-21.137 47.117-47.117 0-26.013-21.137-47.149-47.117-47.149z"/><path d="m464.883 208.867h-417.766c-25.98 0-47.117 21.136-47.117 47.149 0 25.98 21.137 47.117 47.117 47.117h417.766c25.98 0 47.117-21.137 47.117-47.117 0-26.013-21.137-47.149-47.117-47.149z"/><path d="m464.883 353.467h-417.766c-25.98 0-47.117 21.137-47.117 47.149 0 25.98 21.137 47.117 47.117 47.117h417.766c25.98 0 47.117-21.137 47.117-47.117 0-26.012-21.137-47.149-47.117-47.149z"/>
+                        </svg>
+                    </button>
+                </div>
             </div>
-            <div>
-                <button type='button' className='block text-white'>
-                    {/* Icon provided by Freepik */}
-                    <svg className='h-6 w-6 fill-current' id="Layer_1" enable-background="new 0 0 512 512" height="512" viewBox="0 0 512 512" width="512" xmlns="http://www.w3.org/2000/svg">
-                        <path d="m464.883 64.267h-417.766c-25.98 0-47.117 21.136-47.117 47.149 0 25.98 21.137 47.117 47.117 47.117h417.766c25.98 0 47.117-21.137 47.117-47.117 0-26.013-21.137-47.149-47.117-47.149z"/><path d="m464.883 208.867h-417.766c-25.98 0-47.117 21.136-47.117 47.149 0 25.98 21.137 47.117 47.117 47.117h417.766c25.98 0 47.117-21.137 47.117-47.117 0-26.013-21.137-47.149-47.117-47.149z"/><path d="m464.883 353.467h-417.766c-25.98 0-47.117 21.137-47.117 47.149 0 25.98 21.137 47.117 47.117 47.117h417.766c25.98 0 47.117-21.137 47.117-47.117 0-26.012-21.137-47.149-47.117-47.149z"/>
-                    </svg>
-                </button>
-            </div>
+            {open && (
+                <div className='px-8 pt-2 pb-4'>
+                    <a href='#' className='hover:bg-gray-700 rounded px-2 py-1 block text-white font-bold'>Home</a>
+                    <a href='#' className='mt-1 hover:bg-gray-700 rounded px-2 py-1 block text-white font-bold'>About me</a>
+                    <a href='#' className='mt-1 hover:bg-gray-700 rounded px-2 py-1 block text-white font-bold'>Gallery</a>
+                    <a href='#' className='mt-1 hover:bg-gray-700 rounded px-2 py-1 block text-white font-bold'>Experience</a>
+                    <a href='#' className='mt-1 hover:bg-gray-700 rounded px-2 py-1 block text-white font-bold'>Stats</a>
+                </div>
+            )}
         </header>
     );
 }
