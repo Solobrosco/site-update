c19405645eb91589fe94e17d6b8d950e1bcd4ad7
Nikko <nikkosolon24@gmail.com>
2020-08-26 23:35:25 -0700

    Updated links and Title size in Navbar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     return(
         <header className='bg-gray-900'>
             <div className="flex items-center justify-between py-4 px-10">
-                <div className='text-2xl block h-5 text-white'>
+                <div className='text-lg h-5 font-bold text-white'>
                     Nikko A. Solon
                 </div>
                 <div>
@@ -20,11 +20,11 @@ const NavBar = () => {
             </div>
             {open && (
                 <div className='px-8 pt-2 pb-4'>
-                    <a href='#' className='hover:bg-gray-700 rounded px-2 py-1 block text-white font-bold'>Home</a>
-                    <a href='#' className='mt-1 hover:bg-gray-700 rounded px-2 py-1 block text-white font-bold'>About me</a>
-                    <a href='#' className='mt-1 hover:bg-gray-700 rounded px-2 py-1 block text-white font-bold'>Gallery</a>
-                    <a href='#' className='mt-1 hover:bg-gray-700 rounded px-2 py-1 block text-white font-bold'>Experience</a>
-                    <a href='#' className='mt-1 hover:bg-gray-700 rounded px-2 py-1 block text-white font-bold'>Stats</a>
+                    <a href='#Home' className='hover:bg-gray-700 rounded px-2 py-1 block text-white font-semibold'>Home</a>
+                    <a href='#About' className='mt-1 hover:bg-gray-700 rounded px-2 py-1 block text-white font-semibold'>About me</a>
+                    <a href='#Gallery' className='mt-1 hover:bg-gray-700 rounded px-2 py-1 block text-white font-semibold'>Gallery</a>
+                    <a href='#Experience' className='mt-1 hover:bg-gray-700 rounded px-2 py-1 block text-white font-semibold'>Experience</a>
+                    <a href='#Stats' className='mt-1 hover:bg-gray-700 rounded px-2 py-1 block text-white font-semibold'>Stats</a>
                 </div>
             )}
         </header>
