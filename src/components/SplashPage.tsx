e740e655ff94fbe64b2dd5919a0eaf7bedb41848
Nikko <nikkosolon24@gmail.com>
2020-08-27 19:17:06 -0700

    Added static svgs to SplashPage.tsx supporting Parallax

--- src/components/SplashPage.tsx
+++ src/components/SplashPage.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
 
-const SplashPage = () => {
+import Layer0 from '../static/Background.svg'
+import Layer1 from '../static/Stars.svg'
+import Layer2 from '../static/Moon.svg'
+import Layer3 from '../static/MountainLake.svg'
+import Layer4 from '../static/Foreground.svg'
+
+// attribution <a href="https://www.vecteezy.com/free-vector/nature">Nature Vectors by Vecteezy</a>
+
+interface SplashPageProps {
+    text: string;
+}
+
+const SplashPage = (props: SplashPageProps) => {
     return(
-        <Parallax pages={1} scrolling={true}>
-            <ParallaxLayer>
-                <div className="bg-blue-900 text-center text-white">
+        <Parallax pages={3} scrolling={true}>
+            <ParallaxLayer offset={0} speed={1}>
+                <img src={Layer0} alt='background'></img>
+            </ParallaxLayer>
+            <ParallaxLayer offset={0} speed={1}>
+                <img src={Layer1} alt='wut'></img>
+            </ParallaxLayer>
+            <ParallaxLayer offset={0} speed={2}>
+                <img src={Layer2} alt='the'></img>
+            </ParallaxLayer>
+            <ParallaxLayer offset={0} speed={3}>
+                <img src={Layer3} alt='wut'></img>
+            </ParallaxLayer>
+            <ParallaxLayer offset={0} speed={4}>
+                <img src={Layer4} alt='is'></img>
+            </ParallaxLayer>
+            <ParallaxLayer offset={0.3} speed={3}>
+                <div className="text-bold text-center text-white justify-center text-6xl items-center h-10">
                     <h1>
-                    Aspire to inspire before we expire.
+                        {props.text}
                     </h1>
                 </div>
             </ParallaxLayer>
