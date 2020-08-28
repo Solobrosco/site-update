924a05c0db04296b5a358a3734cad06a85a0f495
Nikko <nikkosolon24@gmail.com>
2020-08-27 19:17:24 -0700

    Added splash page component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import './styles/main.css';
+
 import NavBar from './components/NavBar';
+import SplashPage from './components/SplashPage';
 
 function App() {
   return (
-    <div className="flex-col justify-center items-center">
+    <div className="justify-center items-center">
       <NavBar />
-      <div className="bg-blue-900 text-white">Splash Page</div>
+      <SplashPage text={'Aspire to inspire before we expire.'}/>
       <div className="bg-blue-800 text-white">My Room</div>
       <div className="bg-blue-700 text-white">Check me out</div>
       <div className="bg-blue-600 text-white">Pictures</div>
       <div className="bg-blue-500 text-white">Education/Experience</div>
       <div className="bg-blue-400 text-white">Stats(optional)</div>
-      <div className="bg-blue-300 text-white">Footer</div>
+      <div className="bg-blue-300 text-white">Footer Created with React typescript, tailwind and spring.</div>
+      <div className="whitespace-pre-line bg-blue-200 text-white h-10">test</div>
     </div>
   );
 }
