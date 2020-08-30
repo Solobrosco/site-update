4853b31bd910eec25faced219d2adf5237122cf0
Nikko <nikkosolon24@gmail.com>
2020-08-29 23:38:57 -0700

    Created About.tsx component

---
+++ src/components/About.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const About = () => {
+    return(
+        <div className='bg-blue-700 text-white text-center items-center justify-center'>
+            <h2 className='text-xl py-3 font-bold'>
+                Check me out!
+            </h2>
+            <p>
+                Here is a list of all the skills and technologies I have been working with.
+            </p>
+            <h3>
+                Technologies
+            </h3>
+            <ul>
+                <li>React</li>
+            </ul>
+            <h3>
+                Skills
+            </h3>
+            <ul>
+                <li>progression bar</li>
+            </ul>
+        </div>
+    );
+}
+
+export default About;
