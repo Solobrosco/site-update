f16a1b977b15d382b53e7e2aeadfe337548d24d1
Nikko <nikkosolon24@gmail.com>
2020-11-10 23:06:43 -0800

    Generates random quote / commented out img

--- src/components/SimpleSplashPage.tsx
+++ src/components/SimpleSplashPage.tsx
@@ -2,11 +2,22 @@ import React from 'react';
 
 import Snelling from '../static/Snelling.png';
 
+import quotes from '../data/quotes.json';
+
+function pickRandomQuote () {
+    let keys = Object.keys(quotes);
+    let randIndex = keys[Math.floor(Math.random() * keys.length)];
+    let item = quotes[keys[randIndex]];
+    console.log(item );
+    return item;
+}
+
 const SimpleSplashPage = () =>{
+    let quote = pickRandomQuote();
     return(
-        <div className="bg-blue-900 text-white" >
-            <div className='text-bold text-center text-white justify-center items-center'>hello</div>
-            <img className='object-contain ' src={Snelling} alt='snelling'></img>
+        <div className="bg-blue-900 text-white text-center items-center justify-center relative" >
+            <h1 className=''>{quote.quote}<br></br>-{quote.author}</h1>
+            {/* <img className='object-contain ' src={Snelling} alt='snelling'/> */}
         </div>
     );
 }
