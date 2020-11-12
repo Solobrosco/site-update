9f422ecc4b7481c83b907a52a7dd960dace304f5
Nikko <nikkosolon24@gmail.com>
2020-11-11 22:13:38 -0800

    Added Skills component to About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import SkillComponent from './Skills';
+
+import skills from '../data/skills.json';
+
 const About = () => {
     return(
         <div className='bg-blue-700 text-white text-center items-center justify-center'>
@@ -9,18 +13,13 @@ const About = () => {
             <p>
                 Here is a list of all the skills and technologies I have been working with.
             </p>
-            <h3>
+            <h3 className='text-xl py-3 font-bold'>
                 Technologies
             </h3>
-            <ul>
-                <li>React</li>
-            </ul>
-            <h3>
+            <h3 className='text-xl py-3 font-bold'>
                 Skills
             </h3>
-            <ul>
-                <li>progression bar</li>
-            </ul>
+            {skills.map((skillitem: any) => <SkillComponent name={skillitem.name as string} rank={skillitem.rating as string}/>)}
         </div>
     );
 }
