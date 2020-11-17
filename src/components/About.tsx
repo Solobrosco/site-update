48473d890b7c0255fec82c7252ccb19a0cfec9b9
Nikko Solon <nikkosolon24@gmail.com>
2020-11-16 16:45:47 -0800

    Imported placeholder and stylized text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,24 +2,30 @@ import React from 'react';
 
 import SkillComp from './Skills';
 
+import ProfilePicture from '../static/placeholder.png';
+
 import skills from '../data/skills.json';
+
 
 const About = () => {
     return(
-        <div className='bg-blue-700 text-white text-center items-center justify-center'>
-            <h2 className='text-xl py-3 font-bold'>
+        <div id='About' className='bg-blue-700 text-white text-center items-center justify-center'>
+            <div className='font-black text-5xl'>
+                About
+                <img className='object-center' src={ProfilePicture} alt='profile' height="200" width="200"/>
+            </div>
+            <h2 className='text-4xl py-3 font-bold'>
                 Check me out!
             </h2>
             <p>
                 Here is a list of technologies and skills I have been working with and obtained.
             </p>
-            <h3 className='text-xl py-3 font-bold'>
-                Technologies
-            </h3>
-            <h3 className='text-xl py-3 font-bold'>
+            <h3 className='text-3xl py-3 font-bold'>
                 Skills
             </h3>
-            {skills.map((skillitem: any) => <SkillComp name={skillitem.name as string} rank={skillitem.rating as string}/>)}
+            <div className='py-5'>
+                {skills.map((skillitem: any) => <SkillComp name={skillitem.name as string} rank={skillitem.rating as string}/>)}
+            </div>
         </div>
     );
 }
