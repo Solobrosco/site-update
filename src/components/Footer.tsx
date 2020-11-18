eaa4897a144dc90eee5bff6b6091a227e5871e91
Nikko Solon <nikkosolon24@gmail.com>
2020-11-17 17:25:03 -0800

    Created Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <div className='text-center text-white bg-blue-900 h-5'>
+            Footer
+        </div>
+    );
+}
+
+export default Footer;
