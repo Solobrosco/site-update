c334245ec5f6d95e394b13d9a2145d9b3c993165
Nikko Solon <nikkosolon24@gmail.com>
2020-11-17 17:25:29 -0800

    Created Stats components

---
+++ src/components/Stats.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Stats = () => {
+    return(
+        <div className='text-center text-white bg-blue-700 h-10'>
+            Stats
+        </div>
+    );
+}
+
+export default Stats;
