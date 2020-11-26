dfe20a9fa3f22858e0ae2132eb9d9c6491f9f50e
Nikko Solon <nikkosolon24@gmail.com>
2020-11-25 21:08:27 -0800

    Wednesday sesh

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,14 @@ import React from 'react';
 const Footer = () => {
     return(
         <div className='text-center text-gruvwhite bg-gruvbg-0'>
-            Footer
+            Created with
+            <li>
+                <ul>React</ul>
+                <ul>Typescript</ul>
+                <ul>Tailwind</ul>
+            </li>
+            Deployed with Github pages
+            <div>version 1.0 | view source</div>
         </div>
     );
 }
