8ad1996640faade649634eda0b0c6e18aeffe251
Solobrosco <nikkosolon24@gmail.com>
2020-12-13 23:25:51 -0800

    changed enable-background to enableBackground

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -35,7 +35,7 @@ const NavBar = (props: NavBarProps) => {
                     <div className='w-1/3 text-right'>
                         <button type='button' className='' onClick={() => setOpen(!open)}>
                             {/* Icon provided by Freepik */}
-                            <svg className='h-6 w-6 fill-current hover:text-graygruv-light' id="Layer_1" enable-background="new 0 0 512 512" height="512" viewBox="0 0 512 512" width="512" xmlns="http://www.w3.org/2000/svg">
+                            <svg className='h-6 w-6 fill-current hover:text-graygruv-light' id="Layer_1" enableBackground="new 0 0 512 512" height="512" viewBox="0 0 512 512" width="512" xmlns="http://www.w3.org/2000/svg">
                                 <path d="m464.883 64.267h-417.766c-25.98 0-47.117 21.136-47.117 47.149 0 25.98 21.137 47.117 47.117 47.117h417.766c25.98 0 47.117-21.137 47.117-47.117 0-26.013-21.137-47.149-47.117-47.149z"/><path d="m464.883 208.867h-417.766c-25.98 0-47.117 21.136-47.117 47.149 0 25.98 21.137 47.117 47.117 47.117h417.766c25.98 0 47.117-21.137 47.117-47.117 0-26.013-21.137-47.149-47.117-47.149z"/><path d="m464.883 353.467h-417.766c-25.98 0-47.117 21.137-47.117 47.149 0 25.98 21.137 47.117 47.117 47.117h417.766c25.98 0 47.117-21.137 47.117-47.117 0-26.012-21.137-47.149-47.117-47.149z"/>
                             </svg>
                         </button>
