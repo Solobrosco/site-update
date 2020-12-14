258578227323329ba4875e30091b15aa546fbe44
Solobrosco <nikkosolon24@gmail.com>
2020-12-13 23:24:48 -0800

    Refomated data for stats

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 
 import stats from '../data/stats.json'
 
@@ -11,24 +11,25 @@ function startToNow (date?: any) {
     let today = new Date();
     date = Date.parse(date);
     let start = new Date(date);
-    let days = Math.round((today.getTime() - start.getTime()) / msDay);
+    let days = Math.round((today.getTime() - start.getTime()) / msDay); 
     return days;
 }
 
-const Stats = () => {
-    const [count, setCount] = React.useState(stats.Updating.default);
-
-    React.useState(() => {
+const Stats = (stat?: StatsProps) => {
+    const [count, setCount] = useState(stats.Updating.default);
+    
+    useState(() => {
         const num = localStorage.getItem("counter");
         if(num){
             setCount(JSON.parse(num));
         };
     }); 
-
-    React.useEffect(() => {
+    
+    useEffect(() => {
         localStorage.setItem("counter", JSON.stringify(count));
     });
 
+    
     return(
         <div className='text-center text-gruvwhite bg-gruvbg-1'>
             <div className='text-3xl font-black'>Stats</div>
