eae352f7eec4ba037c8165f88988dd4ebb1326ad
Nikko Solon <nikkosolon24@gmail.com>
2021-02-28 08:54:53 -0800

    Added alt prop to img components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,19 +9,19 @@ const Footer = () => {
                 Created with
             </div>
             <div className='font-thin'>
-                <img height="150" width="150" src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"></img>
+                <img height="150" width="150" src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="react_logo"></img>
                 React
             </div>
             <div className='font-thin'>
-                <img height="100" width="100" src="https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg"></img>
+                <img height="100" width="100" src="https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg" alt="TS_logo" ></img>
                 Typescript
             </div>
             <div className='font-thin'>
-                <img height="100" width="100" src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg"></img>
+                <img height="100" width="100" src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" alt="tailwindcss_logo" ></img>
                 Tailwind
             </div>
             <div className='font-bold'>
-                <img height="100" width="100" src="https://raw.githubusercontent.com/gist/johan/1007813/raw/a25829510f049194b6404a8f98d22978e8744a6f/octocat.svg"></img>
+                <img height="100" width="100" src="https://raw.githubusercontent.com/gist/johan/1007813/raw/a25829510f049194b6404a8f98d22978e8744a6f/octocat.svg" alt="github_logo" ></img>
                 Deployed with Github pages
             </div >
             <div className='font-thin'>
