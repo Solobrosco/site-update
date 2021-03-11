ef6aae83f70724ea185ce453c2af94503684990e
Nikko Solon <nikkosolon24@gmail.com>
2021-03-10 17:10:58 -0800

    Edited text for Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,9 +20,9 @@ const Footer = () => {
                 <img height="100" width="100" src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" alt="tailwindcss_logo"></img>
                 Tailwind
             </div>
-            <div className='self-end font-bold'>
+            <div className='self-end font-thin'>
                 <img height="100" width="100" src="https://raw.githubusercontent.com/gist/johan/1007813/raw/a25829510f049194b6404a8f98d22978e8744a6f/octocat.svg" alt="github_logo" ></img>
-                Deployed with Github pages
+                Pages
             </div >
             <div className='self-end font-thin'>
                 version {pack.version} | <a className='hover:text-graygruv-light' href='https://github.com/Solobrosco/site-update' target="_blank" rel="noopener noreferrer">source</a>
