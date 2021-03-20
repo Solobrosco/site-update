1ae554ec9fe233ac43ea4aca374cfda27eb6f4d3
Nikko Solon <nikkosolon24@gmail.com>
2021-03-19 23:32:05 -0700

    Depreciated Imgquote component.

--- src/components/Imgquote.tsx
+++ src/components/Imgquote.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import splash from '../static/MRock.jpg'
+
 
 interface ImgquoteProps {
     img?: boolean,
@@ -10,17 +10,8 @@ interface ImgquoteProps {
 
 const Imgquote = (props: ImgquoteProps) => {
     return(
-        <div>
-            <div className='static flex item-stretch'>
-                {props.img && <div className='z-0 absolute'>
-                    <img src={splash} alt='splash' className='object-cover'></img>
-                </div>}
-            </div>
-            <div className='z-10 bg-gruvbg-1 self-auto absolute px-3 font-medium text-2xl'>
-                "{props.quote}"
-                <br></br>
-                <span className='font-thin text-bluegruv-dark'>- {props.author}</span>
-            </div>
+        <div className='relative'>
+            
         </div>
     );
 }
--- src/components/SimpleSplashPage.tsx
+++ src/components/SimpleSplashPage.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
-import Imgquote from './Imgquote';
-
-
+import splash from '../static/MRock.jpg'
 
 // DATA
 // import quotes from '../data/quotes.json';
@@ -24,9 +22,19 @@ const SimpleSplashPage = (props: simpleSplashPageProps) =>{
     let pick = pickRandomQuote(props.quotes);
     let quote = pick.quote;
     let author = pick.author;
+    
     return(
-        <div className="text-gruvwhite text-center" >
-            <Imgquote img={props.img} quote={quote} author={author} />
+        <div className='static'>
+            <div className="relative text-gruvwhite text-center h-5" >
+                <div className="absolute inset-0 bg-cover bg-center z-0">
+                    {props.img && <img src={splash} alt='splash' className='object-cover'></img>}
+                </div>
+                <div className='items-center inset-0 z-10 flex justify-center absolute bg-blue-900 px-3 font-bold text-4xl'>
+                    "{quote}"
+                    <br></br>
+                    <span className='font-thin text-bluegruv-dark'>- {author}</span>
+                </div>
+            </div>
         </div>
     );
 }
