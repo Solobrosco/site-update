4573226cd5aaf1d105ec02bcb9c0b96929d4850d
Nikko Solon <nikkosolon24@gmail.com>
2021-03-19 23:49:48 -0700

    Imported test pdf and enabled download button

--- src/components/Downloads.tsx
+++ src/components/Downloads.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import pdf from '../data/testDoc.pdf';
+
 interface DownloadsProps {
     showResume?: boolean,
     showCertificates?: boolean,
@@ -8,9 +10,9 @@ interface DownloadsProps {
 const Downloads = (props: DownloadsProps) => {
     return (
         <div className='flex flex-col space-y-3 p-3 justify-center text-center bg-gruvbg-4 text-gruvwhite'>
-            {props.showResume && <button className="hover:text-gruvfg-3 text-3xl bg-gruvbg-2 hover:bg-gruvbg-5 font-black p-5 border-b-4 border-gruvbg-3 hover:border-gruvbg-2 rounded">
+            {props.showResume && <a href={pdf} target='blank' className="hover:text-gruvfg-3 text-3xl bg-gruvbg-2 hover:bg-gruvbg-5 font-black p-5 border-b-4 border-gruvbg-3 hover:border-gruvbg-2 rounded">
                     Download Resume
-                </button>}
+                </a>}
             {props.showCertificates && <button className="text-3xl bg-gruvbg-2 hover:text-gruvfg-3 hover:bg-gruvbg-5 font-black p-5 border-b-4 border-gruvbg-3 hover:border-gruvbg-2 rounded">
                     View Certificates
                 </button>}
