9d46d22fcc76f2b4ff74f71ddfd672a3143b1bef
Solobrosco <nikkosolon24@gmail.com>
2021-07-27 20:22:31 -0700

    Centered absolute div over img

--- src/components/SimpleSplashPage.tsx
+++ src/components/SimpleSplashPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CSS from 'csstype'
 
 // DATA
 // import quotes from '../data/quotes.json';
@@ -38,16 +39,25 @@ const randomPicture = () => {
     return path + batch[randIndex]
 }
 
+const styles: CSS.Properties = {
+    position: "absolute",
+    top: '50%', 
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    textShadow: '5px 3px 3px #282828'
+
+}
+
 const SimpleSplashPage = (props: simpleSplashPageProps) =>{
     let quote = pickRandomQuote(props.quotes);
     return(
-        <div className="relative bg-gruvbg-1 flex text-gruvwhite text-center">
-            <div className='absolute top-1/2 left-1/2 bg-gruvbg-1px-3 font-medium text-2xl'>
+        <div className="relative bg-gruvbg-1 text-gruvwhite text-center">
+            <div style={styles} className='p-3 font-medium text-4xl'>
                 "{quote.quote}"
                 <br></br>
-                <span className='font-thin text-bluegruv-dark'>- {quote.author}</span>
+                <span className='text-2xl text-bluegruv-dark'>- {quote.author}</span>
             </div>
-            {props.img && <img src={randomPicture()} alt='splash' className='object-cover'></img>}
+            {props.img && <img src={randomPicture()} alt='splash' className='object-cover shadow-inner-2xl shadow-lg'></img>}
         </div>
     );
 }
